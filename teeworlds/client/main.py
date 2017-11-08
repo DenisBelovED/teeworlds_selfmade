7d4bc3ddfeccedb95df781185d08f7957059a17d
Denis Belov <aksion2011@yandex.ru>
2017-11-08 08:39:36 +0500

    backup

--- teeworlds/client/main.py
+++ teeworlds/client/main.py
@@ -5,15 +5,23 @@ from multiprocessing import Process
 host = '127.0.0.1'
 port = 2056
 
+controller_proc = None
+
+def start_generating_controller_event(host, port):
+    controller_proc = Process(target=Controller, args=(host, port))
+
+def prepare_exit():
+    controller_proc.terminate()
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((300, 300), 0, 32)
-    controller_proc = Process(target=Controller, args=(host, port))
+
 
     #TODO visualiser class
 
     controller_proc.start()
-    #controller_proc.terminate()
+    #prepare_exit()
 
 if __name__ == '__main__':
     main()
